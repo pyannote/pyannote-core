7e626837cf811043fc9620c642ccea562fa19a91
Herve Bredin <bredin@limsi.fr>
2014-06-02 11:39:47 +0200

    style: PEP8

--- pyannote/core/__init__.py
+++ pyannote/core/__init__.py
@@ -12,8 +12,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -38,8 +38,8 @@ PYANNOTE_SCORE = 'score'
 PYANNOTE_IDENTITY = 'identity'
 
 from time import T, TStart, TEnd
-from segment import Segment
+from segment import Segment, SlidingWindow
 from timeline import Timeline
-from annotation import Annotation
+from annotation import Annotation, Unknown
 from transcription import Transcription
 from scores import Scores
