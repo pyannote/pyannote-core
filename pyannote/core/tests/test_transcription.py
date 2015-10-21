d9e86c2a9b8e66081c13b84c8ca4473625b43ff8
Hervé BREDIN <bredin@limsi.fr>
2015-10-21 14:44:39 +0200

    fix: edges_iter does not guarantee chronological order, ordered_edges_iter does

--- pyannote/core/tests/test_transcription.py
+++ pyannote/core/tests/test_transcription.py
@@ -7,8 +7,8 @@ def test_creation():
     transcription.add_edge(5, 5.5)
     transcription.add_edge(5.5, 7, speech="hi. I'm Leonard", speaker='Leonard')
 
-    assert list(transcription.edges_iter(data=True)) == [(3.0, 5.0, {'speech': "hi there, I'm Penny", 'speaker': 'Penny'}),
-                                                         (5.0, 5.5, {}),
-                                                         (5.5, 7.0, {'speech': "hi. I'm Leonard", 'speaker': 'Leonard'})]
+    assert list(transcription.ordered_edges_iter(data=True)) == [(3.0, 5.0, {'speech': "hi there, I'm Penny", 'speaker': 'Penny'}),
+                                                                 (5.0, 5.5, {}),
+                                                                 (5.5, 7.0, {'speech': "hi. I'm Leonard", 'speaker': 'Leonard'})]
 
     assert transcription.temporal_sort() == [3.0, 5.0, 5.5, 7.0]
