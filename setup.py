c6156cc40c1fafb644cfe05b4222d13d2680973e
Hervé Bredin <bredin@limsi.fr>
2018-11-19 22:22:15 +0100

    setup: remove support for Python 2

--- setup.py
+++ setup.py
@@ -61,11 +61,9 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering"
     ],
 )
