fc103f6a53f4d0651fa39e6b999bddebbf93ee79
FrancescoBonzi <fra.bonzi@gmail.com>
2022-03-11 11:50:33 +0100

    write annotation to lab file supported

--- pyannote/core/annotation.py
+++ pyannote/core/annotation.py
@@ -187,7 +187,7 @@ class Annotation:
         # key: label
         # value: timeline
         self._labels: Dict[Label, Timeline] = {}
-        self._labelNeedsUpdate: [Label, bool] = {}
+        self._labelNeedsUpdate: Dict[Label, bool] = {}
 
         # timeline meant to store all annotated segments
         self._timeline: Timeline = None
@@ -394,6 +394,31 @@ class Annotation:
             line = (
                 f"SPEAKER {uri} 1 {segment.start:.3f} {segment.duration:.3f} "
                 f"<NA> <NA> {label} <NA> <NA>\n"
+            )
+            file.write(line)
+
+    def write_lab(self, file: TextIO):
+        """Dump annotation to file using LAB format
+
+        Parameters
+        ----------
+        file : file object
+
+        Usage
+        -----
+        >>> with open('file.lab', 'w') as file:
+        ...     annotation.write_lab(file)
+        """
+
+        for segment, _, label in self.itertracks(yield_label=True):
+            if isinstance(label, Text) and " " in label:
+                msg = (
+                    f"Space-separated LAB file format does not allow labels "
+                    f'containing spaces (got: "{label}").'
+                )
+                raise ValueError(msg)
+            line = (
+                f"{segment.start:.3f} {segment.start + segment.duration:.3f} {label}\n"
             )
             file.write(line)
 
